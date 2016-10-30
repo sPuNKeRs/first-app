c3629f698550eb4954aef5043c713d6fc8f2261e
PuNKeR <maximkoritin@gmail.com>
2016-10-30 19:52:07 +0300

    30-10-2016

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Root Component</h1>
+    <app-databinding></app-databinding>
+  `
 })
 export class AppComponent {
-  title = 'app works!';
 }
---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent {
+  stringInterpolation = "This is string interpolation!";
+  numberInterpolation = 2;
+
+  onTest(){
+    return true;
+  }
+
+  onClicked(value: string){
+    alert(value);
+  }
+
+}
